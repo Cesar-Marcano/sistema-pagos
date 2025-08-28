b058300b15f4dbd70d883adc669fc3daea06060c
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 00:40:19 -0400

    feat: integrate Passport configuration and middleware into the application

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import logger from "./logger";
+import { container } from "../config/container";
+import { PassportConfig } from "../config/passport";
 
 export async function run() {
   const app = express();
+
+  const passportConfig = container.get<PassportConfig>(PassportConfig);
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  passportConfig.setup();
 
   return app.listen(3000, () => {
     logger.info("Server listeining on port 3000");
