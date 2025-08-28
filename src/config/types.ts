132187de7a1236698b40a7688d79716f693400d0
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 02:05:31 -0400

    feat: organize TYPES constants with added spacing for clarity

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,4 +1,9 @@
 export const TYPES = {
   Prisma: Symbol.for("Prisma"),
+
+  // Services
   IHasherService: Symbol.for("HasherService"),
+
+  // Features
+  UserFeature: Symbol.for("UserFeature"),
 };
