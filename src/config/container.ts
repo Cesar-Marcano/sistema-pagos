e300de16332366718f8c8f566a9bdb68b9c5d70b
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 00:40:31 -0400

    feat: add PrismaClient binding to the dependency injection container

--- src/config/container.ts
+++ src/config/container.ts
@@ -1,9 +1,17 @@
 import { Container } from "inversify";
 import { Argon2Service, IHasherService } from "../services/argon2.service";
 import { TYPES } from "./types";
+import { PassportConfig } from "./passport";
+import { PrismaClient } from "@prisma/client";
 
 const container = new Container();
 
+const prisma = new PrismaClient();
+container.bind<PrismaClient>(TYPES.Prisma).toConstantValue(prisma);
+
+// Services
 container.bind<IHasherService>(TYPES.IHasherService).to(Argon2Service);
+
+container.bind<PassportConfig>(PassportConfig).toSelf();
 
 export { container };
