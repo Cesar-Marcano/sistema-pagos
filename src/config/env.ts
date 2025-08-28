0f993c4fbd43dbbd4a6376f637dd5401c7c099d1
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 17:50:56 -0400

    feat: added token lifetime validation with zod

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 
 import { z } from "zod";
 import logger from "../app/logger";
+import ms, { StringValue } from "ms";
 
 const envSchema = z.object({
   NODE_ENV: z
@@ -10,7 +11,17 @@ const envSchema = z.object({
   PORT: z.string().transform(Number).default(3000),
   JWT_SECRET: z.string().min(1),
   DATABASE_URL: z.url(),
-  TOKEN_LIFETIME: z.string().default("4h"),
+  TOKEN_LIFETIME: z
+    .string()
+    .refine(
+      (val) => typeof val === "string" && ms(val as StringValue) !== undefined,
+      {
+        message:
+          "TOKEN_LIFETIME debe ser un formato de tiempo válido (ej. 1h, 30m, 5d).",
+      }
+    )
+    .default("4h"),
+
   TOKEN_ISSUER: z.string().default("sistema-pagos-estudiantiles"),
 });
 
