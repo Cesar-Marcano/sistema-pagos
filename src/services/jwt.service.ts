2da8916ab5ac1dbd4c0fad8651350a14c7dbdee6
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 18:21:27 -0400

    feat: created sign and verify methods in JwtService

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,27 +1,41 @@
-import { User } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 import { v4 } from "uuid";
-import { sign } from "jsonwebtoken";
+import { sign, verify } from "jsonwebtoken";
 import { StringValue } from "ms";
+import { inject } from "inversify";
+import { TYPES } from "../config/types";
+import createHttpError from "http-errors";
 
 type TokenUserData = Omit<User, "password">;
-type TokenData = TokenUserData & { jti: string };
 
 export class JwtService {
   private readonly jwtSecret: string = process.env.JWT_SECRET;
-  private readonly tokenLifetime: StringValue = process.env.TOKEN_LIFETIME as StringValue;
+  private readonly tokenLifetime: StringValue = process.env
+    .TOKEN_LIFETIME as StringValue;
   private readonly tokenIssuer: string = process.env.TOKEN_ISSUER;
 
-  public sign(userData: TokenUserData): string {
+  public sign(userData: TokenUserData): { token: string; jti: string } {
     const jti = v4();
 
-    const paylaod: TokenData = { ...userData, jti };
-
-    const token = sign(paylaod, this.jwtSecret, {
+    const token = sign(userData, this.jwtSecret, {
       expiresIn: this.tokenLifetime,
       issuer: this.tokenIssuer,
       subject: String(userData.id),
+      jwtid: jti,
     });
 
-    return token;
+    return { token, jti };
+  }
+
+  public verify(token: string): TokenUserData & { jti: string } {
+    try {
+      const payload = verify(token, this.jwtSecret, {
+        issuer: this.tokenIssuer,
+      }) as TokenUserData & { jti: string };
+
+      return payload;
+    } catch (error) {
+      throw createHttpError(403, "Token inválido.");
+    }
   }
 }
