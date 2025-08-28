97f08bd2616260b37182485aa623abb85cc33664
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 18:24:48 -0400

    feat: created ITokenService interface

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -8,8 +8,13 @@ import createHttpError from "http-errors";
 
 type TokenUserData = Omit<User, "password">;
 
+interface ITokenService {
+  sign(userData: TokenUserData): { token: string; jti: string };
+  verify(token: string): TokenUserData & { jti: string };
+}
+
 @injectable()
-export class JwtService {
+export class JwtService implements ITokenService {
   private readonly jwtSecret: string = process.env.JWT_SECRET;
   private readonly tokenLifetime: StringValue = process.env
     .TOKEN_LIFETIME as StringValue;
