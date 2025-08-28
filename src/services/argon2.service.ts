a183e8924e31dcbc52883da460bb6eb4675fe88e
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 00:30:00 -0400

    refactor: refactor password hashing and verification into Argon2Service class

--- src/services/argon2.service.ts
+++ src/services/argon2.service.ts
@@ -3,29 +3,33 @@ import { argon2Options } from "../config/argon2";
 import createHttpError from "http-errors";
 import logger from "../app/logger";
 
-export async function hashPassword(password: string): Promise<string> {
-  try {
-    const hash = await argon2.hash(password, argon2Options);
-
-    return hash;
-  } catch (err) {
-    logger.error("Error al hashear la contraseña", err);
-    throw createHttpError(500, "Error al hashear la contraseña");
-  }
+export interface IHasherService {
+  hash(password: string): Promise<string>;
+  compare(hash: string, password: string): Promise<boolean>;
 }
 
-export async function verifyPassword(
-  hash: string,
-  password: string
-): Promise<boolean> {
-  try {
-    if (await argon2.verify(hash, password)) {
-      return true;
-    } else {
-      return false;
+export class Argon2Service implements IHasherService {
+  public async hash(password: string): Promise<string> {
+    try {
+      const hash = await argon2.hash(password, argon2Options);
+
+      return hash;
+    } catch (err) {
+      logger.error("Error al hashear la contraseña", err);
+      throw createHttpError(500, "Error al hashear la contraseña");
     }
-  } catch (err) {
-    logger.error("Error al verificar la contraseña", err);
-    throw createHttpError(500, "Error al verificar la contraseña");
+  }
+
+  public async compare(hash: string, password: string): Promise<boolean> {
+    try {
+      if (await argon2.verify(hash, password)) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      logger.error("Error al verificar la contraseña", err);
+      throw createHttpError(500, "Error al verificar la contraseña");
+    }
   }
 }
