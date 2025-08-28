546d498fd8d5e5a684490cf933cf87e595dacd11
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 18:21:37 -0400

    feat: create session feature

---
+++ src/features/session.feature.ts
@@ -0,0 +1,41 @@
+import { inject, injectable } from "inversify";
+import { TYPES } from "../config/types";
+import { PrismaClient, Session } from "@prisma/client";
+import { StringValue } from "ms";
+
+@injectable()
+export class SessionFeature {
+  constructor(
+    @inject(TYPES.Prisma)
+    private readonly prisma: PrismaClient
+  ) {}
+
+  public async createSession(
+    userId: number,
+    jti: string,
+    expiration: StringValue
+  ): Promise<Session> {
+    return await this.prisma.session.create({
+      data: {
+        jti,
+        expiration,
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+  }
+
+  public async deleteSession(userId: number, jti: string) {
+    return await this.prisma.session.delete({
+      where: {
+        jti,
+        user: {
+          id: userId,
+        },
+      },
+    });
+  }
+}
