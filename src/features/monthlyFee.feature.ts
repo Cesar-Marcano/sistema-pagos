28f4bb21c56221414577309174b595c54e6a2a40
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-29 08:57:04 -0400

    feat: created monthly fee feature methods

--- src/features/monthlyFee.feature.ts
+++ src/features/monthlyFee.feature.ts
@@ -1,7 +1,12 @@
 import { inject, injectable } from "inversify";
 import { TYPES } from "../config/types";
-import { PrismaClient } from "@prisma/client";
+import { MonthlyFee, PrismaClient } from "@prisma/client";
 import createHttpError from "http-errors";
+import {
+  SearchArgs,
+  SearchResult,
+  searchWithPaginationAndCriteria,
+} from "../lib/search";
 
 @injectable()
 export class MonthlyFeeFeature {
@@ -37,5 +42,81 @@ export class MonthlyFeeFeature {
         },
       },
     });
+  }
+
+  public async update(id: number, description: string) {
+    const existentMonthlyFeeWithDescriptionCount =
+      await this.prisma.monthlyFee.count({
+        where: {
+          description,
+          deletedAt: null,
+        },
+      });
+
+    if (existentMonthlyFeeWithDescriptionCount > 0)
+      throw createHttpError(
+        409,
+        "Ya hay una mensualidad con la misma descripción."
+      );
+
+    return await this.prisma.monthlyFee.update({
+      where: { id, deletedAt: null },
+      data: {
+        description,
+      },
+    });
+  }
+
+  public async softDelete(id: number) {
+    return await this.prisma.monthlyFee.update({
+      where: {
+        id,
+        deletedAt: null,
+      },
+      data: {
+        updatedAt: new Date(),
+      },
+    });
+  }
+
+  public async hardDelete(id: number) {
+    return await this.prisma.monthlyFee.delete({
+      where: {
+        id,
+        deletedAt: {
+          not: null,
+        },
+      },
+    });
+  }
+
+  public async findById(id: number, includeDeleted: boolean) {
+    return await this.prisma.monthlyFee.findUnique({
+      where: {
+        id,
+        ...(includeDeleted ? {} : { deletedAt: null }),
+      },
+    });
+  }
+
+  public async search(
+    args: SearchArgs<
+      Partial<
+        Omit<MonthlyFee, "id" | "deletedAt" | "createdAt" | "updatedAt">
+      > & {
+        deletedAt?: {
+          not: null;
+        };
+      }
+    >
+  ): Promise<SearchResult<MonthlyFee>> {
+    return searchWithPaginationAndCriteria(
+      this.prisma.monthlyFee.findMany,
+      this.prisma.monthlyFee.count,
+      {
+        ...args,
+        where: { ...args.where, deletedAt: null },
+      }
+    );
   }
 }
