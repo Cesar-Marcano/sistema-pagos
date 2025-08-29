2d5b4477a8f6232fe300ba199a242d090ed43805
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 21:55:19 -0400

    refactor: created createSearchController helper

---
+++ src/lib/searchController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { z } from "zod";
+import { container } from "../config/container";
+import { ServiceIdentifier } from "inversify";
+
+export function createSearchController<Feature extends { search: Function }>(
+  zodCriteria: z.ZodObject<any>,
+  feature: ServiceIdentifier<Feature>,
+  whereMapper: (queryParams: any) => object
+) {
+  const defaultSearchSchema = z.object({
+    page: z.string().optional().default("1").transform(Number),
+    pageSize: z.string().optional().default("10").transform(Number),
+    ...zodCriteria.shape,
+  });
+
+  return async function (req: Request, res: Response) {
+    const queryParams = defaultSearchSchema.parse(req.query);
+    const whereClause = whereMapper(queryParams);
+
+    const searchArgs = {
+      page: queryParams.page,
+      pageSize: queryParams.pageSize,
+      where: whereClause,
+    };
+
+    const schoolYearFeature = container.get<Feature>(feature);
+    const schoolYears = await schoolYearFeature.search(searchArgs);
+
+    res.json({ schoolYears });
+  };
+}
