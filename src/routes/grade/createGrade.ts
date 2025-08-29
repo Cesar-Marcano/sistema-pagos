a15faade31d8bdf28513cba65c392e413769171a
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 22:32:13 -0400

    feat: created createGrade controller

---
+++ src/routes/grade/createGrade.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { TYPES } from "../../config/types";
+import { container } from "../../config/container";
+import { z } from "zod";
+import { GradeFeature } from "../../features/grade.feature";
+
+const createGradeSchema = z.object({
+  name: z.string().trim().toUpperCase().min(3),
+});
+
+export async function createGrade(req: Request, res: Response) {
+  const { name } = createGradeSchema.parse(req.body);
+
+  const GradeFeature = container.get<GradeFeature>(TYPES.GradeFeature);
+
+  const grade = await GradeFeature.create(name);
+
+  res.json({ grade });
+}
