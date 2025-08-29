5cf1992a78ccb84a3cace7a5a35113c59b716bf1
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 21:55:33 -0400

    feat: implemented createSearchControllerHelper in search school year controller

--- src/routes/schoolyear/searchSchoolYear.ts
+++ src/routes/schoolyear/searchSchoolYear.ts
@@ -1,36 +1,31 @@
 import { Request, Response } from "express";
-import { TYPES } from "../../config/types";
-import { container } from "../../config/container";
 import { SchoolYearFeature } from "../../features/schoolyear.feature";
 import { z } from "zod";
+import { createSearchController } from "../../lib/searchController";
+import { TYPES } from "../../config/types";
 
 const searchSchoolYearSchema = z.object({
-  page: z.string().optional().default("1").transform(Number),
-  pageSize: z.string().optional().default("10").transform(Number),
   name: z.string().optional(),
   startDate: z.string().optional(),
   endDate: z.string().optional(),
 });
 
-export async function searchSchoolYear(req: Request, res: Response) {
-  const queryParams = searchSchoolYearSchema.parse(req.query);
+const schoolYearSearchCriteria = z.object({
+  name: z.string().optional(),
+  startDate: z.string().optional(),
+  endDate: z.string().optional(),
+});
 
-  const searchArgs = {
-    page: queryParams.page,
-    pageSize: queryParams.pageSize,
-    where: {
-      ...(queryParams.name && { name: queryParams.name }),
-      ...(queryParams.startDate && {
-        startDate: new Date(queryParams.startDate),
-      }),
-      ...(queryParams.endDate && { endDate: new Date(queryParams.endDate) }),
-    },
-  };
+const schoolYearWhereMapper = (queryParams: any) => ({
+  ...(queryParams.name && { name: queryParams.name }),
+  ...(queryParams.startDate && {
+    startDate: new Date(queryParams.startDate),
+  }),
+  ...(queryParams.endDate && { endDate: new Date(queryParams.endDate) }),
+});
 
-  const schoolYearFeature = container.get<SchoolYearFeature>(
-    TYPES.SchoolYearFeature
-  );
-  const schoolYears = await schoolYearFeature.search(searchArgs);
-
-  res.json({ schoolYears });
-}
+export const searchSchoolYear = createSearchController(
+  schoolYearSearchCriteria,
+  TYPES.SchoolYearFeature,
+  schoolYearWhereMapper
+);
