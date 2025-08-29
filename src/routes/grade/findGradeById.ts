b2c6ec6b0a74eee5a6f82eac835cc5b5f1c70b49
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 22:33:36 -0400

    feat: created find grade by id controller

---
+++ src/routes/grade/findGradeById.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { TYPES } from "../../config/types";
+import { container } from "../../config/container";
+import z from "zod";
+import { GradeFeature } from "../../features/grade.feature";
+
+const paramsSchema = z.object({
+  includeDeleted: z
+    .boolean("Include deleted debe ser tipo boolean.")
+    .default(false),
+});
+
+export async function findGradeById(req: Request, res: Response) {
+  const gradeFeature = container.get<GradeFeature>(TYPES.GradeFeature);
+
+  const queryParams = paramsSchema.parse(req.query);
+
+  const grade = await gradeFeature.findGradeById(
+    Number(req.params.id),
+    queryParams.includeDeleted
+  );
+
+  res.json({ grade });
+}
