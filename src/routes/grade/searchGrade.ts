65613b69cd2ba2056a6321004f275c8270a6442f
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 22:34:44 -0400

    feat: created search grade controller

---
+++ src/routes/grade/searchGrade.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { z } from "zod";
+import { createSearchController } from "../../lib/searchController";
+import { TYPES } from "../../config/types";
+
+const gradeSearchCriteria = z.object({
+  name: z.string().optional(),
+});
+
+const gradeWhereMapper = (queryParams: any) => ({
+  ...(queryParams.name && { name: queryParams.name }),
+});
+
+export const searchGrade = createSearchController(
+  gradeSearchCriteria,
+  TYPES.SchoolYearFeature,
+  gradeWhereMapper
+);
