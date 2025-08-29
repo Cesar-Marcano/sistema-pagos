739c8b4b14176912d1797d32da4a448a65d52f65
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 22:37:45 -0400

    feat: created update grade controller

---
+++ src/routes/grade/updateGrade.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { TYPES } from "../../config/types";
+import { container } from "../../config/container";
+import { z } from "zod";
+import { GradeFeature } from "../../features/grade.feature";
+
+const updateGradeSchema = z.object({
+  name: z.string().trim().toUpperCase().min(3),
+});
+
+export async function updateSchoolYear(req: Request, res: Response) {
+  const validatedData = updateGradeSchema.parse(req.body);
+
+  const gradeFeature = container.get<GradeFeature>(TYPES.GradeFeature);
+
+  const grade = await gradeFeature.updateGrade(
+    Number(req.params.id),
+    validatedData.name
+  );
+
+  res.json({ schoolYear: grade });
+}
