b58648eb7d073d05fd4bea1e6acff01a6f8ee948
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-28 22:38:53 -0400

    feat: registered grade router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userRoutes } from "./user";
 import { schoolYearRoutes } from "./schoolyear";
+import { gradeRoutes } from "./grade";
 
 export const router: Router = Router();
 
 router.use("/user", userRoutes);
 router.use("/schoolYear", schoolYearRoutes);
+router.use("/grade", gradeRoutes);
