ee6c49ebba2f3b43653287ee95356587a2227c95
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-29 07:58:15 -0400

    feat: create student feature

---
+++ src/features/student.feature.ts
@@ -0,0 +1,117 @@
+import { inject, injectable } from "inversify";
+import { TYPES } from "../config/types";
+import { PrismaClient, Student } from "@prisma/client";
+import createHttpError from "http-errors";
+import {
+  SearchArgs,
+  SearchResult,
+  searchWithPaginationAndCriteria,
+} from "../lib/search";
+
+@injectable()
+export class StudentFeature {
+  constructor(@inject(TYPES.Prisma) private readonly prisma: PrismaClient) {}
+
+  public async create(name: string): Promise<Student> {
+    const existingStudentCount = await this.prisma.student.count({
+      where: {
+        name,
+        deletedAt: null,
+      },
+    });
+
+    if (existingStudentCount > 0)
+      throw createHttpError(
+        409,
+        "Ya se ha registrado un estudiante con el mismo nombre."
+      );
+
+    return await this.prisma.student.create({
+      data: {
+        name,
+      },
+    });
+  }
+
+  public async update(id: number, name: string): Promise<Student> {
+    const student = await this.prisma.student.findUnique({
+      where: { id, deletedAt: null },
+    });
+
+    if (!student) throw createHttpError(404, "Estudiante no encontrado.");
+
+    if (student.name === name) return student;
+
+    const studentNameCollisions = await this.prisma.student.count({
+      where: { name, deletedAt: null },
+    });
+
+    if (studentNameCollisions > 0)
+      throw createHttpError(
+        409,
+        "Ya existe un estudiante con ese mismo nombre."
+      );
+
+    return await this.prisma.student.update({
+      where: {
+        id,
+        deletedAt: null,
+      },
+      data: {
+        name,
+      },
+    });
+  }
+
+  public async softDelete(id: number): Promise<Student> {
+    return await this.prisma.student.update({
+      where: {
+        id,
+        deletedAt: null,
+      },
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+  }
+
+  public async hardDelete(id: number): Promise<Student> {
+    return await this.prisma.student.delete({
+      where: {
+        id,
+        deletedAt: null,
+      },
+    });
+  }
+
+  public async findById(
+    id: number,
+    includeDeleted: boolean
+  ): Promise<Student | null> {
+    return await this.prisma.student.findUnique({
+      where: {
+        id,
+        ...(includeDeleted ? {} : { deletedAt: null }),
+      },
+    });
+  }
+
+  public async search(
+    args: SearchArgs<
+      Partial<Omit<Student, "id" | "deletedAt" | "createdAt" | "updatedAt">> & {
+        deletedAt?: {
+          not: null;
+        };
+      }
+    >
+  ): Promise<SearchResult<Student>> {
+    return searchWithPaginationAndCriteria(
+      this.prisma.student.findMany,
+      this.prisma.student.count,
+      {
+        ...args,
+        where: { ...args.where, deletedAt: null },
+      }
+    );
+  }
+}
