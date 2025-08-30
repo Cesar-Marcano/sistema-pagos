ea14183ffc48b6618e7d630fd8cc98eebca9b0e4
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-29 21:50:27 -0400

    feat: created payment method methods for CRUD operations

--- src/features/paymentMethod.feature.ts
+++ src/features/paymentMethod.feature.ts
@@ -1,6 +1,12 @@
 import { inject, injectable } from "inversify";
 import { TYPES } from "../config/types";
 import { PaymentMethod, PrismaClient } from "@prisma/client";
+import createHttpError from "http-errors";
+import {
+  SearchArgs,
+  SearchResult,
+  searchWithPaginationAndCriteria,
+} from "../lib/search";
 
 @injectable()
 export class PaymentMethodFeature {
@@ -11,6 +17,20 @@ export class PaymentMethodFeature {
     requiresManualVerification: boolean,
     requiresReferenceId: boolean
   ): Promise<PaymentMethod> {
+    const existentPaymentMethodsWithNameCount =
+      await this.prisma.paymentMethod.count({
+        where: {
+          name,
+          deletedAt: null,
+        },
+      });
+
+    if (existentPaymentMethodsWithNameCount > 0)
+      throw createHttpError(
+        409,
+        "Ya existe un metodo de pago con el nombre ingresado."
+      );
+
     return await this.prisma.paymentMethod.create({
       data: {
         name,
@@ -18,5 +38,90 @@ export class PaymentMethodFeature {
         requiresReferenceId,
       },
     });
+  }
+
+  public async update(id: number, name: string): Promise<PaymentMethod> {
+    const existentPaymentMethodsWithNameCount =
+      await this.prisma.paymentMethod.count({
+        where: {
+          name,
+          id: {
+            not: id,
+          },
+          deletedAt: null,
+        },
+      });
+
+    if (existentPaymentMethodsWithNameCount > 0)
+      throw createHttpError(
+        409,
+        "Ya existe un metodo de pago con el nombre ingresado."
+      );
+
+    return await this.prisma.paymentMethod.update({
+      where: {
+        id,
+        deletedAt: null,
+      },
+      data: {
+        name,
+      },
+    });
+  }
+
+  public async softDelete(id: number): Promise<PaymentMethod> {
+    return await this.prisma.paymentMethod.update({
+      where: {
+        id,
+        deletedAt: null,
+      },
+      data: {
+        deletedAt: null,
+      },
+    });
+  }
+
+  public async hardDelete(id: number): Promise<PaymentMethod> {
+    return await this.prisma.paymentMethod.delete({
+      where: {
+        id,
+        deletedAt: {
+          not: null,
+        },
+      },
+    });
+  }
+
+  public async findbyId(
+    id: number,
+    includeDeleted: boolean
+  ): Promise<PaymentMethod | null> {
+    return await this.prisma.paymentMethod.findUnique({
+      where: {
+        id,
+        ...(includeDeleted ? {} : { deletedAt: null }),
+      },
+    });
+  }
+
+  public async search(
+    args: SearchArgs<
+      Partial<
+        Omit<PaymentMethod, "id" | "deletedAt" | "createdAt" | "updatedAt">
+      > & {
+        deletedAt?: {
+          not: null;
+        };
+      }
+    >
+  ): Promise<SearchResult<PaymentMethod>> {
+    return searchWithPaginationAndCriteria(
+      this.prisma.paymentMethod.findMany,
+      this.prisma.paymentMethod.count,
+      {
+        ...args,
+        where: { ...args.where },
+      }
+    );
   }
 }
