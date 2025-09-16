2d0252b9453679d33eacaf949264a3ce0f569031
Cesar-Marcano <marcanocesar86@gmail.com>
2025-09-16 16:32:26 -0400

    refactor(grade): renamed route and updated params

--- src/routes/grade/findStudentsInGrade.ts
+++ src/routes/grade/findStudentsInGrade.ts
@@ -2,13 +2,13 @@ import { Request, Response } from "express";
 import { TYPES } from "../../config/types";
 import { container } from "../../config/container";
 import { GradeFeature } from "../../features/grade.feature";
-import { FindStudentsByGradeAndYearQueryParams } from "./schemas";
+import { FindStudentsByGradeAndYearParams } from "./schemas";
 
-export async function findStudentsByGradeAndYear(req: Request, res: Response) {
+export async function findStudentsInGrade(req: Request, res: Response) {
   const gradeFeature = container.get<GradeFeature>(TYPES.GradeFeature);
 
-  const { gradeId, schoolYearId } = FindStudentsByGradeAndYearQueryParams.parse(
-    req.body
+  const { gradeId, schoolYearId } = FindStudentsByGradeAndYearParams.parse(
+    req.params
   );
 
   const students = await gradeFeature.findStudentsByGradeAndYear(
