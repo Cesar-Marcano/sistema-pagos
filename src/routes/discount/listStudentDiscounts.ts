80a2fa68fb94e5c465aa65b31b0ac17b924e1ba8
Cesar-Marcano <marcanocesar86@gmail.com>
2025-09-16 16:35:26 -0400

    refactor(discount): validates id param

--- src/routes/discount/listStudentDiscounts.ts
+++ src/routes/discount/listStudentDiscounts.ts
@@ -2,13 +2,18 @@ import { Request, Response } from "express";
 import { TYPES } from "../../config/types";
 import { container } from "../../config/container";
 import { DiscountFeature } from "../../features/discount.feature";
+import { z } from "zod";
 
 export async function listStudentDiscounts(req: Request, res: Response) {
   const discountFeature = container.get<DiscountFeature>(TYPES.DiscountFeature);
 
-  const studentDiscounts = await discountFeature.listStudentDiscounts(
-    Number(req.params.id)
-  );
+  const { id } = z
+    .object({
+      id: z.string().transform(Number).pipe(z.number()),
+    })
+    .parse(req.params);
+
+  const studentDiscounts = await discountFeature.listStudentDiscounts(id);
 
   res.status(200).json({ studentDiscounts });
 }
